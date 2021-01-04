d0adb2dfcbcbcb63ca835d3618faa8cb0dc23a26
xenups <xenups@gmail.com>
2021-01-04 19:07:21 +0330

    movie detail repo added

--- lib/repositories/movie_details.dart
+++ lib/repositories/movie_details.dart
@@ -2,7 +2,7 @@ import 'package:bloc_mvvm_tutorial/models/movie.dart';
 import 'package:bloc_mvvm_tutorial/networking/api_helper.dart';
 
 class MovieDetailRepository {
-  final String _apiKey = "key";
+  final String _apiKey = "2ad3176cbb73bbb38c64d807d6abe7fb";
   ApiBaseHelper _helper = ApiBaseHelper();
 
   Future<MovieDetail> fetchMovieDetail(int selectedMovie) async {

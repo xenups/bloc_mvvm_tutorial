b7d11bc1d6234c59b055865a5e77b3d992c51cae
xenups <xenups@gmail.com>
2021-01-04 19:06:13 +0330

    movielist view added

---
+++ lib/views/movie_list.dart
@@ -0,0 +1,140 @@
+import 'package:bloc_mvvm_tutorial/blocs/movie_bloc.dart';
+import 'package:bloc_mvvm_tutorial/models/movie.dart';
+import 'package:bloc_mvvm_tutorial/networking/api_responses.dart';
+import 'package:flutter/material.dart';
+
+import 'movie_details.dart';
+
+class MovieScreen extends StatefulWidget {
+  @override
+  _MovieScreenState createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  MovieListBloc _bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = MovieListBloc();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text(
+          'Mover',
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => _bloc.fetchMovieList(),
+        child: StreamBuilder<ApiResponse<MovieList>>(
+          stream: _bloc.movieListStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              switch (snapshot.data.status) {
+                case Status.LOADING:
+                  return Loading(loadingMessage: snapshot.data.message);
+                  break;
+                case Status.COMPLETED:
+                  print(snapshot.data.data);
+                  print(snapshot.data.data.totalPages);
+                  snapshot.data.data.results.forEach((element) {
+                    print(element.title);
+                  });
+                  // return MovieScreen(movieListView: snapshot.data.data);
+                  break;
+                case Status.ERROR:
+                  return Error(
+                    errorMessage: snapshot.data.message,
+                    onRetryPressed: () => _bloc.fetchMovieList(),
+                  );
+                  break;
+              }
+            }
+            return Container();
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+}
+
+class Error extends StatelessWidget {
+  final String errorMessage;
+
+  final Function onRetryPressed;
+
+  const Error({Key key, this.errorMessage, this.onRetryPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            errorMessage,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 8),
+          RaisedButton(
+            color: Colors.redAccent,
+            child: Text(
+              'Retry',
+              style: TextStyle(
+//                color: Colors.white,
+                  ),
+            ),
+            onPressed: onRetryPressed,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  final String loadingMessage;
+
+  const Loading({Key key, this.loadingMessage}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            loadingMessage,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+//              color: Colors.lightGreen,
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(height: 24),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.lightGreen),
+          ),
+        ],
+      ),
+    );
+  }
+}

8a221cefc6b829ae856725ffd51e4f708bc415ed
xenups <xenups@gmail.com>
2021-01-04 19:05:43 +0330

    Fixed MovieListBloc

--- lib/blocs/movie_bloc.dart
+++ lib/blocs/movie_bloc.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 import 'package:bloc_mvvm_tutorial/models/movie.dart';
 import 'package:bloc_mvvm_tutorial/networking/api_responses.dart';
 import 'package:bloc_mvvm_tutorial/repositories/movie_list.dart';
+import 'package:flutter/material.dart';
 
-class MovieBloc {
+class MovieListBloc {
   MovieListRepository _movieListRepository;
+
   StreamController _movieListController;
 
   StreamSink<ApiResponse<MovieList>> get movieListSink =>
@@ -13,10 +15,17 @@ class MovieBloc {
   Stream<ApiResponse<MovieList>> get movieListStream =>
       _movieListController.stream;
 
+  MovieListBloc() {
+    _movieListController = StreamController<ApiResponse<MovieList>>();
+    _movieListRepository = MovieListRepository();
+    fetchMovieList();
+  }
+
   fetchMovieList() async {
-    movieListSink.add(ApiResponse.loading("Fetching movies"));
+    movieListSink.add(ApiResponse.loading('Fetching Movies'));
     try {
       MovieList movies = await _movieListRepository.fetchMovieList();
+      print(movies);
       movieListSink.add(ApiResponse.completed(movies));
     } catch (e) {
       movieListSink.add(ApiResponse.error(e.toString()));
@@ -24,9 +33,8 @@ class MovieBloc {
     }
   }
 
-  MovieBloc() {
-    fetchMovieList();
+  dispose() {
+    _movieListController?.close();
   }
-
-  dispose() {}
 }
+
